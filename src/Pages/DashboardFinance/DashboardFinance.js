b0ed9012b3699de819e52c05902f1106eeb5de47
Stéphane Dauguet <sdauguet@gmail.com>
2022-11-17 15:52:12 +0100

    add static chart bar

--- src/Pages/DashboardFinance/DashboardFinance.js
+++ src/Pages/DashboardFinance/DashboardFinance.js
@@ -1,10 +1,22 @@
+import BarChart from "../../Components/AllChart/BarChart";
 import "./DashboardFinance.css";
 
 const DashboardFinance = () => {
   return (
-    <div>
-      <h1>FINANCES</h1>
-      <h1>FINANCES</h1>
+    <div className="global-container">
+      <h1>Les résultats de l'année : [2020]</h1>
+      <form>
+        <label htmlFor="year">Choisissez une date</label>
+        <select id="year">
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+        </select>
+      </form>
+      <div className="dasboard-container">
+        <BarChart />
+      </div>
     </div>
   );
 };
